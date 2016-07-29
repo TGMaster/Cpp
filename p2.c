3e8a48b8179eacbdfe0cb1c26358067bc200431d
TG <TGMaster@users.noreply.github.com>
2016-07-30 01:08:10 +0700

    Update fix bug

--- p2.c
+++ p2.c
@@ -13,23 +13,17 @@ void shiftToEnd(int arr[], int n, int max)
 
 void main(int arg, char *str[])
 {
-	int max = 0, temp, counti, array[128];
+	int max = 0, temp, i, array[128];
 	if (arg > 1)
 	{
 		for (int i = 1; i < arg; i++)
 		{
+			max = (max < atoi(str[i])) ? atoi(str[i]) : max;
 			array[i] = atoi(str[i]);
 		}
-		for (int i = 0; array[i] != 0; i++)
-		{
-			counti++;
-			if (max < atoi(str[i])) {
-				max = array[i];
-			}
-		}
-		shiftToEnd(array, counti, max);
-		for (int i = 1; i < (counti-1); i++)
-			printf("%d ",array[i]);
+		shiftToEnd(array, i, max);
+		for (int j = 1; i < i; j++)
+			printf("%d ",array[j]);
 		printf("\n");
 	}
 }
