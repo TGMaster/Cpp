34bbbf69c0267ad9f3741988df415f9266e90d95
TG <TGMaster@users.noreply.github.com>
2016-08-25 23:41:24 +0700

    Create tamgiacnguoc.cpp

---
+++ VeHinh/tamgiacnguoc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    for(i=rows;i>=1;--i)
+    {
+        for(j=1;j<=i;++j)
+        {
+           cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
